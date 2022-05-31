4c710438a9471909b4ba8f5b343dc22907fbde76
Alfonso Reyes <vaca.hace.muu@gmail.com>
2022-05-31 10:45:14 -0600

    new rofi theme, update polybar colors

--- .config/rofi/menus/powermenu.sh
+++ .config/rofi/menus/powermenu.sh
@@ -5,7 +5,7 @@
 ## Github  : @adi1090x
 ## Twitter : @adi1090x
 
-dir="~/.config/rofi/themes/material"
+dir="$HOME/.config/rofi/themes/material"
 uptime=$(uptime -p | sed -e 's/up //g')
 
 rofi_command="rofi -theme $dir/powermenu.rasi"
